e31371937c4db30e917a069fced1852ddb3a353e
Briac Ferte <briac.ferte@gmail.com>
2021-02-07 22:47:55 +0100

    07/02/21

--- App.js
+++ App.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, TextInput, Image, ScrollView} from 'react-native';
+
+import cow from './assets/cow.jpg'
+
 
 const App = () => {
-  
-    return <View style={{
-      borderWidth: 1,
-      height: 150,
-      width: 150,
-      backgroundColor: "green"
+  const [state, setState] = useState('');
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.hello}>Hello World</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="entrer du texte"
+          value={state}
+          onChangeText={(value) => setState(value)}
+        />
+        <Image source={cow} style={styles.cow}/>
+        <Image source={cow} style={styles.cow}/>
+        <Image source={cow} style={styles.cow}/>
+        <Image source={cow} style={styles.cow}/>
+        <Image source={cow} style={styles.cow}/>
+        <Image source={cow} style={styles.cow}/>
+        <Image source={cow} style={styles.cow}/>
+        <Image source={cow} style={styles.cow}/>
+        <Image source={cow} style={styles.cow}/>
+        <Image source={cow} style={styles.cow}/>
+      </View>
+    </ScrollView>
+  );
+};
 
-
-    }}></View>
-  
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderWidth: 1,
+    backgroundColor: 'green',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20
+  },
+  hello: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10
+  },
+  input: {
+    borderWidth: 1,
+    width: 200,
+    height: 40,
+    color: '#fff',
+  },
+  cow: {
+    height: 150,
+    width: 150,
+    marginTop:10
+  }
+});
 
 export default App;
