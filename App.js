c9d80a427a3c3fc3e9a50242d2ff70e29f0e9e56
Briac Ferte <briac.ferte@gmail.com>
2021-02-07 21:54:50 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const App = () => {
+  
+    return <View style={{
+      borderWidth: 1,
+      height: 150,
+      width: 150,
+      backgroundColor: "green"
+
+
+    }}></View>
+  
+}
+
+export default App;
